25c4c77810de7815e1332dd35d1ae9f44ae11956
Valdir Júnior <valdirmsjunior@gmail.com>
2024-05-08 16:21:19 -0300

    adicionando sqlserver

--- obj/Debug/net8.0/desafio-api-net.AssemblyInfo.cs
+++ obj/Debug/net8.0/desafio-api-net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desafio-api-net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b2dd3a01d9bff5914a5484877e4e56a086ac626")]
 [assembly: System.Reflection.AssemblyProductAttribute("desafio-api-net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desafio-api-net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
